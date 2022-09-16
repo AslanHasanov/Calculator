b1ee555915d2c22894868e4a85feb1408415ab1f
Aslan R. Hasanov <aslanrh@code.local>
2022-09-16 20:59:52 +0400

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+function plus(){
+    var num1=document.getElementById("num1").value
+    var num2=document.getElementById("num2").value
+    // var result=parseInt(num1)+parseInt(num2)
+    // var result=(+num1)+(+num2)
+    console.log(result)
+    document.getElementById("result").innerHTML=result
+    
+}
